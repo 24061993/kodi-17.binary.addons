a78e5d3cbed301252a1664a2267e47153cc188c2
vdrtuxnet <felix.l.kamps@gmail.com>
2016-10-02 22:07:04 +0200

    update Repo

---
+++ repo/update.sh
@@ -0,0 +1,5 @@
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/Generic_x68_64
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/OSX_64
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi2
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/WIN_32
