5ef9369b9f75eecd18ad1782b3b9737d010dbe41
vdrtuxnet <felix.l.kamps@gmail.com>
2016-10-03 00:21:05 +0200

    update Repo

--- repo/update.sh
+++ repo/update.sh
@@ -2,4 +2,4 @@
 /www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/OSX_64
 /www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi
 /www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi2
-/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/WIN_32
+/src/kodi/kodi-17.binary.addons/repo/repo_aktualisieren.sh /src/kodi/kodi-17.binary.addons/repo/WIN_32
